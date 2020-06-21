f759b2a7a7e807e5c7992701399b46405d4c8bdb
pr0found <cairunqiang@gmail.com>
2020-06-21 19:10:57 +0800

    rename

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,52 +1,56 @@
-import Taro, { Component, useState, useEffect } from '@tarojs/taro'
 import { View } from '@tarojs/components'
 import { AtList, AtListItem, AtLoadMore } from 'taro-ui'
-import { IPartyList }  from './../../utils/interface'
 import { loading, hiddenLoading, toast } from './../../utils/modal'
+import Taro, { Component } from '@tarojs/taro'
+
 import './index.scss'
 
-const PAGE_SIZE: number = 10
+const db = wx.cloud.database()
 
-function List(props): JSX.Element {
-  const [partyList, setPartyList] = useState<Array<IPartyList>>([])
-  const [curPage, setCurPage] = useState<number>(0)
-  const [hasMore, setHasMore] = useState<boolean>(true)
-  const [isLoading, setIsLoading] = useState<boolean>(false)
+export default class PartyListView extends Component {
+  constructor(){
+    super()
+    this.state = {
+      partyList: [],
+      curIdx: 0
+    }
+  }
 
-  const fetchPartyList = () => {
-    !hasMore && toast('没有更多了', 'none', 2000)
+  fetchPartyList = () => {
     loading('加载中')
-    setIsLoading(true)
-    const db = wx.cloud.database()
-    db.collection('party').skip(curPage * PAGE_SIZE).limit(10).get().then(res => {
+    db.collection('party').skip(this.state.curIdx).limit(20).get().then(res => {
       hiddenLoading()
-      setIsLoading(false)
       const { data } = res
-      setPartyList([...partyList, ...data])
-      if (data.length < 10) {
-        setHasMore(false)
-      }
+      let newList = this.state.partyList
+      newList.push.apply(newList, data)
+      this.setState(
+        {
+          partyList: newList
+        }
+      )
     }).catch(() => {
       toast('出了点问题', 'none', 1000)
     })
   }
 
-  const joinParty  = (partyId) => {
+  joinParty  = (partyId) => {
     return Taro.navigateTo({
       url: '/pages/party/index?id=' + partyId
     })
   }
 
-  useEffect(() => {
-    fetchPartyList()
-  }, [curPage])
-
-  const loadMore = () => {
-    hasMore && setCurPage(curPage + 1)
+  loadMore = () => {
+    const ci = this.state.curIdx
+    this.setState({
+      curIdx : ci+1
+    })
+    this.fetchPartyList()
   }
 
-  return (
-    <View className="list-apge">
+  return () {
+    const { partyList } = this.state
+
+    return (<View className="list-apge">
       
       <View className="list-body">
         <AtList>
@@ -60,20 +64,19 @@ function List(props): JSX.Element {
                 note={item.date.toLocaleString()}
                 thumb={item.sponsorAvatarUrl}
                 disabled={item.complete}
-                onClick = {() => {joinParty(item._id)}}
+                onClick = {() => {this.joinParty(item._id)}}
                 />
               )
             })
           }
         </AtList>
         <AtLoadMore
-          customStyle={isLoading ? {display: 'none'} : {}}
-          onClick={loadMore}
-          status={hasMore ? 'more' : 'noMore'}
+          onClick={this.loadMore}
+          status='more'
         />
       </View>
     </View>
-  )
-}
+    )
+  }
 
-export default List
+}
